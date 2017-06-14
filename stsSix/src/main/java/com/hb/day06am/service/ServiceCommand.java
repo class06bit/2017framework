3809cacbc3c7db5353c5874d3d76b1bc1f5ab176
1027 <1027@hb6000>
2017-06-14 10:15:18 +0900

    mybatis - dao구현체가 없는 실행

---
+++ stsSix/src/main/java/com/hb/day06am/service/ServiceCommand.java
@@ -0,0 +1,10 @@
+package com.hb.day06am.service;
+
+import java.sql.SQLException;
+
+import org.apache.ibatis.session.SqlSession;
+
+public interface ServiceCommand {
+
+	//void sCmd(SqlSession sqlSession) throws SQLException;
+}
